6aa4c01419f711832b3aa794126f1abf7e03911a
Viniciuswps <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2018-06-19 16:27:43 -0300

    pequena alteracao

--- MatematicaDiscreta/fecho/fecho_reflexivo.py
+++ MatematicaDiscreta/fecho/fecho_reflexivo.py
@@ -3,7 +3,7 @@
 '''
 Recebe como parâmetros um conjunto no qual a relação é aplicada e a própria relação.
 '''
-def fecho_reflexiva(A, relacao):
+def fecho_reflexivo(A, relacao):
     for elemento in A:
         if not (elemento, elemento) in relacao:
             relacao.append((elemento, elemento))
@@ -19,10 +19,10 @@ R5 = [(1, 1),(1, 2),(1, 3),(1, 4),(2, 2),(2, 3),(2, 4),(3, 3),(3, 4),(4, 4)]
 
 # Testando ...
 
-assert fecho_reflexiva(A,R1) == [(1, 1),(1, 2),(2, 1),(2, 2),(3, 4),(4, 1),(4, 4),(3, 3)]
-assert fecho_reflexiva(A,R2) == [(1, 1),(1, 2),(2, 1),(2, 2),(3, 3),(4, 4)]
-assert fecho_reflexiva(A,R3) == R3
-assert fecho_reflexiva(A,R4) == [(2, 1),(3, 1),(3, 2),(4, 1),(4, 2),(4, 3),(1, 1),(2, 2),(3, 3),(4, 4)]
-assert fecho_reflexiva(A,R5) == R5
+assert fecho_reflexivo(A,R1) == [(1, 1),(1, 2),(2, 1),(2, 2),(3, 4),(4, 1),(4, 4),(3, 3)]
+assert fecho_reflexivo(A,R2) == [(1, 1),(1, 2),(2, 1),(2, 2),(3, 3),(4, 4)]
+assert fecho_reflexivo(A,R3) == R3
+assert fecho_reflexivo(A,R4) == [(2, 1),(3, 1),(3, 2),(4, 1),(4, 2),(4, 3),(1, 1),(2, 2),(3, 3),(4, 4)]
+assert fecho_reflexivo(A,R5) == R5
 
 
