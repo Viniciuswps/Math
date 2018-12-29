2ef3f0af0ab0be79ef1cdba14da09741b25e552f
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2018-12-29 14:46:25 -0300

    add output

--- TeoremaBinomial/binomioDeNewton.go
+++ TeoremaBinomial/binomioDeNewton.go
@@ -43,8 +43,26 @@ func teoremaBinomial(a,n int) string{
 }
 
 func main() {
+	imprimeTrianguloDePascal(10)
+	// output
+	// 1
+	// 1 1
+	// 1 2 1
+	// 1 3 3 1
+	// 1 4 6 4 1
+	// 1 5 10 10 5 1
+	// 1 6 15 20 15 6 1
+	// 1 7 21 35 35 21 7 1
+	// 1 8 28 56 70 56 28 8 1
+	// 1 9 36 84 126 126 84 36 9 1
+
 	fmt.Println(teoremaBinomial(2,0))
 	fmt.Println(teoremaBinomial(2,1))
 	fmt.Println(teoremaBinomial(2,2))
 	fmt.Println(teoremaBinomial(2,3))
+	// output
+	// (x + 2)^0 =  1 * x^0
+	// (x + 2)^1 =  1 * x^1   2 * x^0
+	// (x + 2)^2 =  1 * x^2   4 * x^1   4 * x^0
+	// (x + 2)^3 =  1 * x^3   6 * x^2   12 * x^1   8 * x^0
 }
